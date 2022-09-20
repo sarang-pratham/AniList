82b80ceb6865e77adafa39dfaac0baf3db7929e3
Sarang Pratham <sarangpratham720@gmail.com>
2022-09-20 19:17:40 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <div>App</div>
+    </BrowserRouter>
+  )
+}
+
+export default App
