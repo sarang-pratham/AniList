935ec541070d787d3f4693a3ea27dd68ab6287a4
Sarang Pratham <sarangpratham720@gmail.com>
2022-09-24 09:37:09 +0530

    custom grid temp cols

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,14 @@ module.exports = {
         "eerie-black": '#252422',
         "flame-orange": '#EB5E28'
       },
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(100px, 1fr))",
+        "auto-fill": "repeat(auto-fit, minmax(100px, 1fr))",
+      },
+      gridTemplateRows: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fit, minmax(0, 1fr))",
+      }
     },
   },
   plugins: [],
