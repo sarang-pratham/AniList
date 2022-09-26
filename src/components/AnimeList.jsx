184a66145fbf7da3a48e7b608fd80c3e227dea4a
Sarang Pratham <sarangpratham720@gmail.com>
2022-09-26 18:32:02 +0530

    added load more functionality

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import Card from './Card'
 
 const AnimeList = () => {
+  const {type} = useParams()
+  const [animData, setAnimData] = useState([])
+  const [pageNum, setPageNum] = useState(1)
+  let url = ''
+
+  switch(type){
+    case "top":
+      url = `https://api.jikan.moe/v4/top/anime?page=${pageNum}`
+      break
+    case "now":
+      url = `https://api.jikan.moe/v4/seasons/now?page=${pageNum}`
+      break
+    case "upcoming":
+      url = `https://api.jikan.moe/v4/seasons/upcoming?page=${pageNum}`
+      break 
+  }
+
+  const getData = async(url)=>{
+    const response = await fetch(url)
+    const data = await response.json()
+    setAnimData([...animData, ...data.data])
+  }
+  
+  useEffect(()=>{
+    getData(url)
+  },[pageNum])
+  
   return (
-    <div>AnimeList</div>
+    <>
+    <div className='mt-20 container mx-auto grid grid-cols-5 gap-4 text-floral-white'>
+     {
+      animData && animData.map(anime => (<Card data={anime} key={anime.mal_id}/>))
+     } 
+    </div>
+    <button onClick={()=> setPageNum(pageNum+1)} className='relative inset-2/4 translate-x-[-50%] hover:bg-floral-white border text-floral-white border-floral-white hover:text-eerie-black p-3 my-5 text-lg font-bold rounded'>Load More</button>
+    
+    </>
   )
 }
 
